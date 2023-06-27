c42b752214d7f62678f4963f0852199c858a61ca
Trevor Gaffney <tgaffer@gmail.com>
2023-06-27 20:59:02 +0900

    Stupid Coach Made

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:question]
+    @answer = if @question[-1] == '?'
+                'Silly question, get dressed and go to work!'
+              elsif @question == 'I am going to work'
+                'Great!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
+end
